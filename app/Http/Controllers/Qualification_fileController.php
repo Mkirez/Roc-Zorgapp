8d88b71a9c4c48ab51897c09afee375c136a18ca
Diamé Morreau <diamemorreau@me.com>
2021-01-15 13:39:33 +0100

    controller benaming qf

--- app/Http/Controllers/Qualification_fileController.php
+++ app/Http/Controllers/Qualification_fileController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-class Qualification_fileController extends Controller
+class QualificationFileController extends Controller
 {
     public function index()
     {
